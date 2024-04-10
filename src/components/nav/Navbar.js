6d5f9e6e9b02a4695f1413ac7b6d2261c008bf2b
Romaric Zounlome <rzounlom@gmail.com>
2024-04-09 20:00:57 -0400

    complete navbar and nav links

--- src/components/nav/Navbar.js
+++ src/components/nav/Navbar.js
@@ -1,6 +1,28 @@
 import "./Navbar.css";
+
+import CreatePostModal from "../modals/CreatePostModal";
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav>
+      {" "}
+      <div className="logo">
+        <Link to="/">MTB</Link>
+      </div>
+      <ul>
+        <li>
+          <Link to="/posts">Posts</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+        <li>
+          <CreatePostModal />
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
